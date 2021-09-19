1044091d140a617910464c81fce567e087bf5277
mac <mac@192.168.10.18>
2021-09-19 17:50:04 -0500

    mi primer commit

---
+++ src/main/java/com/tienda/mintic/MiPrimerRepocitorioGitApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.mintic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerRepocitorioGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerRepocitorioGitApplication.class, args);
+	}
+
+}
